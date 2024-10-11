697e92fbaa08403168c6d1111f2af7d2dad42ae0
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-10-12 02:36:27 +0400

    Task2_Gaphrindashvili

--- src/test/java/Task2.java
+++ src/test/java/Task2.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("შეიყვანეთ კონუსის რადიუსი (r): ");
+        double r = scanner.nextDouble();
+
+        System.out.print("შეიყვანეთ კონუსის სიმაღლე (h): ");
+        double h = scanner.nextDouble();
+
+        System.out.print("შეიყვანეთ კალთის სიმაღლე (s): ");
+        double s = scanner.nextDouble();
+
+
+        double surfaceArea = Math.PI * r * (r + s);
+
+        double volume = (1.0 / 3) * Math.PI * Math.pow(r, 2) * h;
+
+        System.out.println("კონუსსის ზედაპირის ფართობი: " + surfaceArea);
+        System.out.println("კონუსის მოცულობა: " + volume);
+
+    }
 }
