459afeac3a733c3c829f4fe4633dd9d7debb4176
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-10-12 02:45:50 +0400

    Task4_Gaphrindashvili

--- src/test/java/Task4.java
+++ src/test/java/Task4.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("შეიყვანეთ გაცვლის კურსი (USD-EUR): ");
+        double exchangeRate = scanner.nextDouble();
+
+        System.out.print("შეიყვანეთ თანხა USD-ში: ");
+        double amountUSD = scanner.nextDouble();
+
+        System.out.print("შეიყვანეთ საკომისიო პროცენტი: ");
+        double feePercentage = scanner.nextDouble();
+
+        double eurReceived = amountUSD * exchangeRate * (1 - (feePercentage / 100));
+
+        System.out.println("ევროში მიღებული თანხა: " + eurReceived);
+
+    }
 }
